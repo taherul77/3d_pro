aa51df4e89d805d14ac699d6c387db27c78d4390
taherul77 <taherul2317@gmail.com>
2024-05-31 11:20:40 +0600

    update pro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hhh
+    <main className="container min-h-screen">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    
     </main>
   );
 }
